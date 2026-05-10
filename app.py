e3386ab9ba94159d7bef8a67e19882b937215b4b
artrib <art.rib@gmail.com>
2026-05-10 21:41:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ choice = st.sidebar.radio("Menu de Gestão", menu)
 
 # --- ABA PESQUISAR ---
 if choice == "🔍 Pesquisar":
-    search = st.text_input("", placeholder="Pesquisar...")
+    search = st.text_input("", placeholder="Pesquisa...")
     
     if not df.empty:
         if search:
