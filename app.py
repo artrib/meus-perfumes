e85cf6aed03d563af95e76e938f8493a3582c206
artrib <art.rib@gmail.com>
2026-05-13 20:10:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ if choice == "🔍 Pesquisar":
             df_pie.columns = ["Periodo", "count"]
             
             # Ajuste de cores e tamanho
-            fig_yn = px.pie(df_pie, values='count', names='Periodo', hole=0.55, color_discrete_sequence=['#c7adc9', '#7f8282'])
+            fig_yn = px.pie(df_pie, values='count', names='Periodo', hole=0.55, color_discrete_sequence=['#c7adc9', '#605f78'])
             # Centralização via legenda abaixo e redução de tamanho
             fig_yn.update_layout(
                 showlegend=True, 
