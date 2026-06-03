e497ed2e288187c30a4864532fb4c0fcec028203
artrib <art.rib@gmail.com>
2026-06-03 07:57:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -289,7 +289,7 @@ if choice == " Pesquisar":
                 color="Ambiente",
                 color_discrete_map={
                     "Calor": "#94A684",
-                    "Frio": "#607274"
+                    "Frio": "#f2f07c"
                 }
             )
 
